2d4c1ee2027e9e6074a6dbec06244e07633ef0a9
ABANGTANYI <abangtanyia@gmail.com>
2023-09-07 16:08:52 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the fuction that every program is written inside
+ *
+ * Return: the return should alway be zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+
+}
