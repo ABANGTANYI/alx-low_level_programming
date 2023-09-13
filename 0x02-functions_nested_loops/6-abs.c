eb794d3e057963f196d0c9a2390eb35b9f34036d
ABANGTANYI <abangtanyia@gmail.com>
2023-09-12 19:08:07 -0700

    _abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - Computes the absolute value of an integer
- * n: integer to compute the absolute value of
+ *@n: integer to compute the absolute value of
  *
  * Return: absolute value of n
  */
